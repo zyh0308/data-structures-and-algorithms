4d38e200cc4f96d50b90d3190eb8b2b6493b938b
Sarah <47694725+sajafisher29@users.noreply.github.com>
2020-01-18 19:15:55 -0800

    Update ArrayShiftTest.java
    Your test is failing because it is outputting the address of the array instead of the Array values. Using Array.toString() is one way to get to the content.

--- code401challenges/src/test/java/code401challenges/ArrayShiftTest.java
+++ code401challenges/src/test/java/code401challenges/ArrayShiftTest.java
@@ -9,8 +9,7 @@ public class ArrayShiftTest {
         int[] input = new int[]{1,2,3,4,5};
         int value = 6;
         int[] expected = new int[]{1,2,3,6,4,5};
-        assertArrayEquals("inserted value into middle of array", expected, ArrayShift.insertShiftArray(input, value));
+        assertEquals("inserted value into middle of array", Arrays.toString(expected), Arrays.toString(ArrayShift.insertShiftArray(input, value)));
     }
-
 
 }
