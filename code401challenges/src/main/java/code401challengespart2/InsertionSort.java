152d07100e80f26cc6407939217ee9d46c4e5dba
Zhang <zyihang@38f9d3618f7e.ant.amazon.com>
2020-02-11 11:39:37 -0800

    updated

--- code401challenges/src/main/java/code401challengespart2/InsertionSort.java
+++ code401challenges/src/main/java/code401challengespart2/InsertionSort.java
@@ -1,12 +1,11 @@
 package code401challengespart2;
 
 public class InsertionSort {
-    public void InsertionSort(int arr[])
-    {
-        int n = arr.length;
-        for (int i = 1; i < n; ++i) {
-            int temp = arr[i];
+    public int[] insertionsort(int [] arr){
+
+        for (int i = 1; i < arr.length; ++i) {
             int j = i - 1;
+            int temp = arr[i];
 
             while (j >= 0 && arr[j] > temp) {
                 arr[j + 1] = arr[j];
@@ -14,6 +13,9 @@ public class InsertionSort {
             }
             arr[j + 1] = temp;
         }
+
+        return arr;
+
     }
 
 }
