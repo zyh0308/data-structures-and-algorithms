6c20597d7a03d78c7b1aa3d9bb1df53895e2c5ca
Zhang <zyihang@38f9d3618f7e.ant.amazon.com>
2020-02-12 11:29:00 -0800

    27 done

--- code401challenges/src/main/java/code401challengespart2/blog.md
+++ code401challenges/src/main/java/code401challengespart2/blog.md
@@ -23,6 +23,61 @@ Sample Array: [8,4,23,42,16,15]
 
 ### Efficiency
 
+Time: O(n^2)
+
 
 ### Solution
-![insertion](assets/insertion.jpg)
+![insertion](resources/assets/insertion.jpg)
+
+
+
+## code challenge 27 merge Sort
+
+Merge sort is a divide-and-conquer algorithm based on the idea of breaking down a list into several sub-lists until each sublist consists of a single element and merging those sublists in a manner that results into a sorted list. Idea: Divide the unsorted list into sublists, each containing element.
+### Pseudocode
+   ALGORITHM Mergesort(arr)
+       DECLARE n <-- arr.length
+
+       if n > 1
+         DECLARE mid <-- n/2
+         DECLARE left <-- arr[0...mid]
+         DECLARE right <-- arr[mid...n]
+         // sort the left side
+         Mergesort(left)
+         // sort the right side
+         Mergesort(right)
+         // merge the sorted left and right sides together
+         Merge(left, right, arr)
+
+   ALGORITHM Merge(left, right, arr)
+       DECLARE i <-- 0
+       DECLARE j <-- 0
+       DECLARE k <-- 0
+
+       while i < left.length && j < right.length
+           if left[i] <= right[j]
+               arr[k] <-- left[i]
+               i <-- i + 1
+           else
+               arr[k] <-- right[j]
+               j <-- j + 1
+
+           k <-- k + 1
+
+       if i = left.length
+          set remaining entries in arr to remaining values in right
+       else
+          set remaining entries in arr to remaining values in left
+
+
+### Trace
+Sample Array: [8,4,23,42,16,15]
+
+
+### Efficiency
+
+Time:
+
+
+### Solution
+![merge](resources/assets/27.jpg)
--- code401challenges/src/test/java/code401challengespart2/SortingTest.java
+++ code401challenges/src/test/java/code401challengespart2/SortingTest.java
@@ -5,6 +5,7 @@ import org.junit.Test;
 import static org.junit.Assert.assertArrayEquals;
 
 public class SortingTest {
+    // test for insertion sort
     @Test
     public void testInsertionSortOne() {
         Sorting test = new Sorting();
@@ -14,31 +15,90 @@ public class SortingTest {
         assertArrayEquals(expected, test.InsertionSort(arr));
 
     }
+
     @Test
     public void testInsertionSortTwo() {
         Sorting test = new Sorting();
-        int[] arr = {20,18,12,8,5,-2};
+        int[] arr = {20, 18, 12, 8, 5, -2};
         test.InsertionSort(arr);
         int[] expected = {-2, 5, 8, 12, 18, 20};
         assertArrayEquals(expected, test.InsertionSort(arr));
 
     }
+
     @Test
     public void testInsertionSortThree() {
         Sorting test = new Sorting();
-        int[] arr = {5,12,7,5,5,7};
+        int[] arr = {5, 12, 7, 5, 5, 7};
         test.InsertionSort(arr);
         int[] expected = {5, 5, 5, 7, 7, 12};
         assertArrayEquals(expected, test.InsertionSort(arr));
 
     }
+
     @Test
     public void testInsertionSortFour() {
         Sorting test = new Sorting();
-        int[] arr = {2,3,5,7,13,11};
+        int[] arr = {2, 3, 5, 7, 13, 11};
         test.InsertionSort(arr);
         int[] expected = {2, 3, 5, 7, 11, 13};
         assertArrayEquals(expected, test.InsertionSort(arr));
 
+    }
+
+    //test for merge sort
+
+    @Test
+    public void testMerge() {
+        int actualLength[] = new int[6];
+        int[] left = new int[]{4, 8, 23};
+        int[] right = new int[]{15, 16, 42};
+        Sorting.merge(left, right, actualLength);
+        int[] expected = new int[]{4, 8, 15, 16, 23, 42};
+        assertArrayEquals(expected, actualLength);
+    }
+
+    @Test
+    public void mergeSortTestOne() {
+
+        int[] actual = new int[]{8, 4, 23, 42, 16, 15};
+        Sorting.mergeSort(actual);
+
+        int[] expected = new int[]{4, 8, 15, 16, 23, 42};
+
+        assertArrayEquals(expected, actual);
+    }
+
+    @Test
+    public void mergeSortTestTwo() {
+
+        int[] actual = new int[]{20,18,12,8,5,-2};
+        Sorting.mergeSort(actual);
+
+        int[] expected = new int[]{-2,5,8,12,18,20};
+
+        assertArrayEquals(expected, actual);
+    }
+
+    @Test
+    public void mergeSortTestThree() {
+
+        int[] actual = new int[]{5,12,7,5,5,7};
+        Sorting.mergeSort(actual);
+
+        int[] expected = new int[]{5,5,5,7,7,12};
+
+        assertArrayEquals(expected, actual);
+    }
+
+    @Test
+    public void mergeSortTestFour() {
+
+        int[] actual = new int[]{2,3,5,7,13,11};
+        Sorting.mergeSort(actual);
+
+        int[] expected = new int[]{2,3,5,7,11,13};
+
+        assertArrayEquals(expected, actual);
     }
 }
